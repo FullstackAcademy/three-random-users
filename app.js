b50035fbc3aa2967a737ed92d83ddf744c6c719e
katthartic <katt.baum@gmail.com>
2020-02-01 16:04:11 -0500

    Attempt to dynamically reduce email text size

--- app.js
+++ app.js
@@ -1,6 +1,38 @@
 const randomUsersURL = 'https://acme-users-api-rev.herokuapp.com/api/users/random'
 const cards = document.querySelector('#cards')
+let emails
 
+Promise.all([fetch(randomUsersURL), fetch(randomUsersURL), fetch(randomUsersURL)])
+    .then(response => Promise.all(response.map(result => result.json())))
+    .then(renderUserCard)
+
+function renderUserCard(userArr) {
+    let newCards = userArr.map((user, idx) => {
+        return `
+        <div class="card" data-id="${idx}" style="order: 0">
+            <div class='page'><a href="#${idx}">${idx + 1}</a></div>
+            <div class='user'>
+                <p>${user.fullName}</p>
+                <p class='email'>${user.email}</p>
+                <div class='avatar' style="background-image: url(${user.avatar})"></div>
+            </div>
+        </div>
+        `
+    }).join('')
+
+
+    emails = [...document.getElementsByClassName('email')]
+
+    emails.forEach(email => {
+        if (email.innerHTML.length > 24) email.classList.add('reduce-at-24')
+        console.log(email.innerHTML.length)
+        console.log(email.classList)
+    })
+
+    cards.innerHTML = newCards
+
+    window.location.hash = '#all' //forces hash refresh on reload
+}
 
 window.addEventListener('hashchange', () => {
     const idStr = window.location.hash.slice(1)
@@ -24,26 +56,3 @@ window.addEventListener('hashchange', () => {
         })
     }
 })
-
-
-Promise.all([fetch(randomUsersURL), fetch(randomUsersURL), fetch(randomUsersURL)])
-    .then(response => Promise.all(response.map(result => result.json())))
-    .then(renderUserCard)
-
-
-function renderUserCard(userArr) {
-    let newCards = userArr.map((user, idx) => {
-        return `
-        <div class="card" data-id="${idx}" style="order: 0">
-            <div class='page'><a href="#${idx}">${idx + 1}</a></div>
-            <div class='user'>
-                <p>${user.fullName}</p>
-                <p>${user.email}</p>
-                <div class='avatar' style="background-image: url(${user.avatar})"></div>
-            </div>
-        </div>
-        `
-    }).join('')
-    cards.innerHTML = newCards
-    window.location.hash = '#all' //forces hash refresh on reload
-}
