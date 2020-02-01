348500eb9fa68a97780aa7d0bf9d0db8f3cc1b9d
katthartic <katt.baum@gmail.com>
2020-02-01 11:20:14 -0500

    Add hashchange logical to change classes

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
 const randomUsersURL = 'https://acme-users-api-rev.herokuapp.com/api/users/random'
 const cards = document.querySelector('#cards')
+let users
+
+window.addEventListener('hashchange', () => {
+    const idStr = window.location.hash.slice(1)
+    const idNum = window.location.hash.slice(1) * 1
+    const userCards = [...document.querySelectorAll('.card')]
+
+    if (Number.isNaN(idNum) || idNum >= userCards.length || idNum < 0) {
+        userCards.forEach(card => {
+            card.classList.remove('selected', 'hidden')
+            card.setAttribute('order', '0')
+        })
+    } else {
+        userCards.forEach(card => {
+            if (card.getAttribute('data-id') === idStr) {
+                card.classList.add('selected')
+                card.classList.remove('hidden')
+            } else {
+                card.classList.remove('selected')
+                card.classList.add('hidden')
+            }
+        })
+    }
+})
+
 
 Promise.all([fetch(randomUsersURL), fetch(randomUsersURL), fetch(randomUsersURL)])
     .then(response => Promise.all(response.map(result => result.json())))
-    .then(renderUserCard)
+    .then(data => {
+        users = data
+        renderUserCard(users)
+    })
 
 
 function renderUserCard(userArr) {
@@ -15,7 +43,7 @@ function renderUserCard(userArr) {
 
     let newCards = userArr.map((user, idx) => {
         return `
-        <div class="card">
+        <div class="card" data-id="${idx}" style="order: 0">
             <div class='page'><a href="#${idx}">${idx + 1}</a></div>
             <div class='user'>
                 <p>${user.fullName}</p>
@@ -26,4 +54,5 @@ function renderUserCard(userArr) {
         `
     }).join('')
     cards.innerHTML = newCards
+    window.location.hash = '#all'
 }
