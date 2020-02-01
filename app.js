e4cf4ca71af5731089753b30b3cfbfc595441538
katthartic <katt.baum@gmail.com>
2020-02-01 04:46:14 -0500

    Add renderUserCard with generic info

--- app.js
+++ app.js
@@ -1,2 +1,27 @@
 const randomUsersURL = 'https://acme-users-api-rev.herokuapp.com/api/users/random'
+const cards = document.querySelector('#cards')
 
+Promise.all([fetch(randomUsersURL), fetch(randomUsersURL), fetch(randomUsersURL)])
+    .then(response => Promise.all(response.map(result => result.json())))
+    .then(renderUserCard)
+
+
+function renderUserCard(userArr) {
+    const user1 = userArr[0]
+    const user2 = userArr[1]
+    const user3 = userArr[2]
+
+    let newCards = userArr.map(user => {
+        return `
+        <div class="card">
+            <div class='page'><a href="#1">1</a></div>
+            <div class='user'>
+                <p>Name</p>
+                <p>Email</p>
+                <div class='avatar'></div>
+            </div>
+        </div>
+        `
+    }).join()
+    cards.innerHTML = newCards
+}
